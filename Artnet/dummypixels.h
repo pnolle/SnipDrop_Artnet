5b3b8ba67bf6a8420128c69f33f80a025601a4b7
pnolle <pnolle@web.de>
2023-02-06 23:43:13 +0100

    comments: wrong numbering

--- Artnet/dummypixels.h
+++ Artnet/dummypixels.h
@@ -30,59 +30,118 @@ const int16_t len_p19_5=6;
 const int16_t p19_5[len_p19_5]={85,84,83,82,81,158};
 const int16_t len_p20_5=8;
 const int16_t p20_5[len_p20_5]={80,79,78,159,160,161,162,163};
+
+
+// 21_5
 const int16_t len_p20_6=2;
 const int16_t p20_6[len_p20_6]={77,76};
+
+// 20_6
 const int16_t len_p21_5=4;
 const int16_t p21_5[len_p21_5]={164,165,166,167};
+
+// 21_6
 const int16_t len_p21_6=4;
 const int16_t p21_6[len_p21_6]={75,74,73,72};
+
+// 21_7
 const int16_t len_p22_6=8;
 const int16_t p22_6[len_p22_6]={168,169,170,171,71,70,69,68};
+
+// 21_8
 const int16_t len_p23_6=8;
 const int16_t p23_6[len_p23_6]={172,173,174,175,67,66,65,64};
+
+// 21_9
 const int16_t len_p24_6=7;
 const int16_t p24_6[len_p24_6]={176,177,178,179,63,62,61};
+
+// 21_10
 const int16_t len_p25_6=8;
 const int16_t p25_6[len_p25_6]={180,181,182,183,60,59,58,57};
+
+// 20_11
 const int16_t len_p26_6=9;
 const int16_t p26_6[len_p26_6]={184,185,186,187,188,56,55,54,53};
+
+// 20_12
 const int16_t len_p27_6=8;
 const int16_t p27_6[len_p27_6]={189,190,191,192,52,51,50,49};
+
+// 19_13
 const int16_t len_p28_5=4;
 const int16_t p28_5[len_p28_5]={193,194,195,196};
+
+// 20_13
 const int16_t len_p28_6=4;
 const int16_t p28_6[len_p28_6]={48,47,46,45};
+
+// 19_14
 const int16_t len_p29_5=8;
 const int16_t p29_5[len_p29_5]={197,198,199,200,201,42,41,40};
+
+// 20_14
 const int16_t len_p29_6=2;
 const int16_t p29_6[len_p29_6]={44,43};
+
+// 18_15
 const int16_t len_p30_4=4;
 const int16_t p30_4[len_p30_4]={203,204,205,206};
+
+// 19_15
 const int16_t len_p30_5=5;
 const int16_t p30_5[len_p30_5]={202,39,38,37,36};
-const int16_t len_p30_4=7;
-const int16_t p30_4[len_p30_4]={207,208,35,34,33,32,31};
-const int16_t len_p30_3=3;
-const int16_t p30_3[len_p30_3]={209,210,211};
-const int16_t len_p31_4=1;
-const int16_t p31_4[len_p31_4]={30};
-const int16_t len_p31_3=8;
-const int16_t p31_3[len_p31_3]={212,213,21,29,28,27,26,25};
+
+// 17_16
+const int16_t len_p30b_3=3;
+const int16_t p30b_3[len_p30b_3]={209,210,211};
+
+// 18_16
+const int16_t len_p30b_4=7;
+const int16_t p30b_4[len_p30b_4]={207,208,35,34,33,32,31};
+
+// 16_17
 const int16_t len_p31_2=4;
 const int16_t p31_2[len_p31_2]={215,216,217,218};
-const int16_t len_p32_2=6;
-const int16_t p32_2[len_p32_2]={219,24,23,22,21,20};
-const int16_t len_p32_1=6;
-const int16_t p32_1[len_p32_1]={220,221,222,223,19,18};
+
+// 17_17
+const int16_t len_p31_3=8;
+const int16_t p31_3[len_p31_3]={212,213,21,29,28,27,26,25};
+
+// 18_17
+const int16_t len_p31_4=1;
+const int16_t p31_4[len_p31_4]={30};
+
+// 14_18
 const int16_t len_p32_0=4;
 const int16_t p32_0[len_p32_0]={224,225,226,227};
-const int16_t len_p33_1=2;
-const int16_t p33_1[len_p33_1]={17,16};
-const int16_t len_p33_0=4;
-const int16_t p33_0[len_p33_0]={228,15,14,13,12};
-const int16_t len_p33_m1=8;
-const int16_t p33_m1[len_p33_m1]={229,230,231,232,11,10,9,8}  ;
+
+// 15_18
+const int16_t len_p32_1=6;
+const int16_t p32_1[len_p32_1]={220,221,222,223,19,18};
+
+// 16_18
+const int16_t len_p32_2=6;
+const int16_t p32_2[len_p32_2]={219,24,23,22,21,20};
+
+// 11_19
+const int16_t len_p33_m3=7;
+const int16_t p33_m3[len_p33_m3]={237,238,239,240,3,2,1};
+
+// 12_19
 const int16_t len_p33_m2=8;
 const int16_t p33_m2[len_p33_m2]={233,234,235,236,7,6,5,4};
-const int16_t len_p33_m3=7;
-const int16_t p33_m3[len_p33_m3]={237,238,239,240,3,2,1}  ;
+
+// 13_19
+const int16_t len_p33_m1=8;
+const int16_t p33_m1[len_p33_m1]={229,230,231,232,11,10,9,8};
+
+// 14_19
+const int16_t len_p33_0=5;
+const int16_t p33_0[len_p33_0]={228,15,14,13,12};
+
+// 15_19
+const int16_t len_p33_1=2;
+const int16_t p33_1[len_p33_1]={17,16};
+
+const int8_t numPx = 44;
