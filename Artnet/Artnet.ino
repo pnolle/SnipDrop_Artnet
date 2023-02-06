eb076de774dca5f562148d78db967dee5b2ed2c9
pnolle <pnolle@web.de>
2023-02-07 00:31:39 +0100

    better numbering

--- Artnet/Artnet.ino
+++ Artnet/Artnet.ino
@@ -56,7 +56,8 @@ boolean ConnectWifi(void)
 
 void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* data)
 {
-  Serial.println(printf("\nonDmxFrame #%u uni%u l%u seq%u", frameNo, universe, length, sequence));
+  Serial.println("_");
+  Serial.println(printf("onDmxFrame #%u uni%u l%u seq%u", frameNo, universe, length, sequence));
   sendFrame = 1;
   // set brightness of the whole strip 
   if (universe == 15)
@@ -82,194 +83,206 @@ void onDmxFrame(uint16_t universe, uint16_t length, uint8_t sequence, uint8_t* d
 
       switch (led)
       {
-        case 0:
+        // row 1
+        case 10:
             thisCount = len_p11_1;
             thisRegion = p11_1;
             break;
-        case 1:
+        case 11:
             thisCount = len_p12_1;
             thisRegion = p12_1;
             break;
-        case 2:
+        case 12:
             thisCount = len_p13_1;
             thisRegion = p13_1;
             break;
-        case 3:
+        case 13:
             thisCount = len_p14_1;
             thisRegion = p14_1;
             break;
-        case 4:
-            thisCount = len_p14_2;
-            thisRegion = p14_2;
-            break;
-        case 5:
+        case 14:
             thisCount = len_p15_1;
             thisRegion = p15_1;
             break;
-        case 6:
+
+        // row 2
+        case 23:
+            thisCount = len_p14_2;
+            thisRegion = p14_2;
+            break;
+        case 24:
             thisCount = len_p15_2;
             thisRegion = p15_2;
             break;
-        case 7:
+        case 25:
             thisCount = len_p16_2;
             thisRegion = p16_2;
             break;
-        case 8:
+
+        // row 3
+        case 35:
             thisCount = len_p16_3;
             thisRegion = p16_3;
             break;
-        case 9:
+        case 36:
             thisCount = len_p17_3;
             thisRegion = p17_3;
             break;
-        case 10:
-            thisCount = len_p17_4;
-            thisRegion = p17_4;
-            break;
-        case 11:
+        case 37:
             thisCount = len_p18_3;
             thisRegion = p18_3;
             break;
-        case 12:
+
+        // row 4
+        case 46:
+            thisCount = len_p17_4;
+            thisRegion = p17_4;
+            break;
+        case 47:
             thisCount = len_p18_4;
             thisRegion = p18_4;
             break;
-        case 13:
+        case 48:
             thisCount = len_p19_4;
             thisRegion = p19_4;
             break;
-        case 14:
+
+        // row 5
+        case 58:
             thisCount = len_p19_5;
             thisRegion = p19_5;
             break;
-        case 15:
+        case 59:
             thisCount = len_p20_5;
             thisRegion = p20_5;
             break;
-        case 16:
-            thisCount = len_p20_6;
-            thisRegion = p20_6;
-            break;
-        case 17:
+        case 60:
             thisCount = len_p21_5;
             thisRegion = p21_5;
             break;
-        case 18:
+
+        // row 6
+        case 69:
+            thisCount = len_p20_6;
+            thisRegion = p20_6;
+            break;
+        case 70:
             thisCount = len_p21_6;
             thisRegion = p21_6;
             break;
-        case 19:
-            thisCount = len_p22_6;
-            thisRegion = p22_6;
-            break;
-        case 20:
-            thisCount = len_p23_6;
-            thisRegion = p23_6;
-            break;
-        case 21:
-            thisCount = len_p24_6;
-            thisRegion = p24_6;
-            break;
-        case 22:
-            thisCount = len_p25_6;
-            thisRegion = p25_6;
-            break;
-        case 23:
-            thisCount = len_p26_6;
-            thisRegion = p26_6;
-            break;
-        case 24:
-            thisCount = len_p27_6;
-            thisRegion = p27_6;
-            break;
-        case 25:
-            thisCount = len_p28_5;
-            thisRegion = p28_5;
-            break;
-        case 26:
-            thisCount = len_p28_6;
-            thisRegion = p28_6;
-            break;
-        case 27:
-            thisCount = len_p29_5;
-            thisRegion = p29_5;
-            break;
-        case 28:
-            thisCount = len_p29_6;
-            thisRegion = p29_6;
-            break;
-        case 29:
-            thisCount = len_p30_4;
-            thisRegion = p30_4;
-            break;
-        case 30:
-            thisCount = len_p30_5;
-            thisRegion = p30_5;
-            break;
-        case 31:
-            thisCount = len_p30b_4;
-            thisRegion = p30b_4;
-            break;
-        case 32:
-            thisCount = len_p30b_3;
-            thisRegion = p30b_3;
-            break;
-        case 33:
-            thisCount = len_p31_4;
-            thisRegion = p31_4;
-            break;
-        case 34:
-            thisCount = len_p31_3;
-            thisRegion = p31_3;
-            break;
-        case 35:
-            thisCount = len_p31_2;
-            thisRegion = p31_2;
-            break;
-        case 36:
-            thisCount = len_p32_2;
-            thisRegion = p32_2;
-            break;
-        case 37:
-            thisCount = len_p32_1;
-            thisRegion = p32_1;
-            break;
-        case 38:
-            thisCount = len_p32_0;
-            thisRegion = p32_0;
-            break;
-        case 39:
-            thisCount = len_p33_1;
-            thisRegion = p33_1;
-            break;
-        case 40:
-            thisCount = len_p33_0;
-            thisRegion = p33_0;
-            break;
-        case 41:
-            thisCount = len_p33_m1;
-            thisRegion = p33_m1;
-            break;
-        case 42:
-            thisCount = len_p33_m2;
-            thisRegion = p33_m2;
-            break;
-        case 43:
-            thisCount = len_p33_m3;
-            thisRegion = p33_m3;
-            break;
-      }
 
-      Serial.println(printf("pixel %i | region length %i \n", led, thisCount));
+        // row 7
+        // case 19:
+        //     thisCount = len_p21_7;
+        //     thisRegion = p21_7;
+        //     break;
+        // case 20:
+        //     thisCount = len_p21_8;
+        //     thisRegion = p21_8;
+        //     break;
+        // case 21:
+        //     thisCount = len_p21_9;
+        //     thisRegion = p21_9;
+        //     break;
+        // case 22:
+        //     thisCount = len_p21_10;
+        //     thisRegion = p21_10;
+        //     break;
+        // case 23:
+        //     thisCount = len_p20_11;
+        //     thisRegion = p20_11;
+        //     break;
+        // case 24:
+        //     thisCount = len_p20_12;
+        //     thisRegion = p20_12;
+        //     break;
+        // case 25:
+        //     thisCount = len_p19_13;
+        //     thisRegion = p19_13;
+        //     break;
+        // case 26:
+        //     thisCount = len_p20_13;
+        //     thisRegion = p20_13;
+        //     break;
+        // case 27:
+        //     thisCount = len_p19_14;
+        //     thisRegion = p19_14;
+        //     break;
+        // case 28:
+        //     thisCount = len_p20_14;
+        //     thisRegion = p20_14;
+        //     break;
+        // case 29:
+        //     thisCount = len_p18_15;
+        //     thisRegion = p18_15;
+        //     break;
+        // case 30:
+        //     thisCount = len_p19_15;
+        //     thisRegion = p19_15;
+        //     break;
+        // case 31:
+        //     thisCount = len_p18_16;
+        //     thisRegion = p18_16;
+        //     break;
+        // case 32:
+        //     thisCount = len_p17_16;
+        //     thisRegion = p17_16;
+        //     break;
+        // case 33:
+        //     thisCount = len_p18_17;
+        //     thisRegion = p18_17;
+        //     break;
+        // case 34:
+        //     thisCount = len_p17_17;
+        //     thisRegion = p17_17;
+        //     break;
+        // case 35:
+        //     thisCount = len_p16_17;
+        //     thisRegion = p16_17;
+        //     break;
+        // case 36:
+        //     thisCount = len_p16_18;
+        //     thisRegion = p16_18;
+        //     break;
+        // case 37:
+        //     thisCount = len_p15_18;
+        //     thisRegion = p15_18;
+        //     break;
+        // case 38:
+        //     thisCount = len_p14_18;
+        //     thisRegion = p14_18;
+        //     break;
+        // case 39:
+        //     thisCount = len_p15_19;
+        //     thisRegion = p15_19;
+        //     break;
+        // case 40:
+        //     thisCount = len_p14_19;
+        //     thisRegion = p14_19;
+        //     break;
+        // case 41:
+        //     thisCount = len_p13_19;
+        //     thisRegion = p13_19;
+        //     break;
+        // case 42:
+        //     thisCount = len_p12_19;
+        //     thisRegion = p12_19;
+        //     break;
+        // case 43:
+        //     thisCount = len_p11_19;
+        //     thisRegion = p11_19;
+        //     break;
+      }
 
       for(int l=0; l<thisCount; l++) {
 //        Serial.println(printf("illuminating led %i %i | i %i | data start: %i \n", thisRegion[l], numLeds, i, i * 3));
         leds[thisRegion[l]] = CRGB(data[i * 3], data[i * 3 + 1], data[i * 3 + 2]);
       }
     }
-    else {
-      Serial.println(printf("pixel not illuminated %i \n", led));
-    }
+//    else {
+//    //   Serial.println(printf("pixel not illuminated %i \n", led));
+//      Serial.print(".");
+//    }
   }
   previousDataLength = length;     
   FastLED.show();
